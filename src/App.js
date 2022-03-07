e19fcf73a4cd8ee51ec6ba8db539646858795b05
franco1316 <francoacm1011000@gmail.com>
2022-03-06 19:49:57 -0500

    consuming api-weather

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import './App.css';
+import {useState, useEffect} from 'react';
+import axios from 'axios';
 
 function App() {
+
+  const [weather, setWeather] = useState({})
+
+  const success = (pos) => {
+
+    const latitude = pos.coords.latitude
+    const longitude = pos.coords.longitude
+    const apiKey = 'ab66cda0625cbf49ebc7931f2f93b588'
+
+      axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`)
+      .then(res=>setWeather(res.data))
+      .catch(e=>console.log(e))
+  }
+
+  useEffect(()=>{
+     navigator.geolocation.getCurrentPosition(success)
+  },[])
+
+  console.log(weather)
+ 
   return (
     <div className="App">
-      
+      <img src={` http://openweathermap.org/img/wn/${weather.weather?.[0].icon}@2x.png`} alt="" />
     </div>
   );
 }
